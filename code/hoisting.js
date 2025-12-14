1fb2ff8b906817d2f02e1a52edbfef9ae65ce52b
Sonali Kolambe <sonali.kolambe@pens.com>
2025-12-14 22:49:19 +0530

    hoistingprogram

---
+++ code/hoisting.js
@@ -0,0 +1,13 @@
+// Var varibales automactically goes on the top when file loads
+
+console.log(x);
+//const x=10;  //error - Cannot access 'x' before initialization
+//let x=10;   // error - Cannot access 'x' before initialization
+var x=10;   // output- undefined
+
+
+
+// print()
+// function print(){
+//     console.log("I am learning js");
+// }
