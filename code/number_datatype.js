f463dd915f033cb5258a1137717187509041de88
Sonali Kolambe <sonali.kolambe@pens.com>
2025-12-14 22:40:19 +0530

    firstcommit

---
+++ code/number_datatype.js
@@ -0,0 +1,28 @@
+//Number data type= 64 bit= 8 bit of memmory
+
+// var y=10;               //Leteral way- to define number
+// var x=Number(10);      // object way- using Number class which defines x value as 10
+// console.log(x);
+
+// var a=10;
+// var b= 3;
+// var c=a/b;
+// console.log(c.toFixed(4));
+
+
+var a=10.2;
+var b= 3.2;
+var c= a + b;
+console.log(c);
+
+//var x="10"
+//var x=Number("10abc");          // NaN- Not a Number
+// var x=parseInt("10abc");       // output=100    parseInt takes if string starts with number till character come
+// //var x=parseInt("sd10abc");   //NaN-Not a Number
+// var y= x*x;
+// console.log(y);
+
+// var x=Number.MAX_SAFE_INTEGER;       //includes signed value
+//var x=Number.MAX_VALUE;               //Only maximum number
+// var y=Number.MIN_SAFE_INTEGER;
+// console.log(x==y);
