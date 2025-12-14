679ea0598fee6d2e13523628e8c3d2b9a5da0805
Sonali Kolambe <sonali.kolambe@pens.com>
2025-12-14 23:01:30 +0530

    swapcommit

---
+++ code/swapping.js
@@ -0,0 +1,22 @@
+// Swapping of two number using third variable
+console.log('--------------First way------------')
+var a=10, b=7;
+var c=a;
+a=b;
+b=c
+console.log('value of a= ' +a);
+console.log('value of b= '+b);
+
+
+//swapping of 2 numbers without third number
+console.log('--------------Second way------------')
+var p=30, q=90;
+p=p+q;
+q=p-q;
+p=p-q;
+console.log('value of p= '+p);
+console.log('value of q= '+q);
+
+
+
+
